bbb2c7684e60797cc6fced804fcd9df12cadbfe3
Sheron <sheronroy2003@gmail.com>
2024-12-17 12:01:38 +0530

    Login page UI update

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1;
+    EditText et1,et2;
+    String uname,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        b1=(Button) findViewById(R.id.logbt);
+        et1=(EditText) findViewById(R.id.uname);
+        et2=(EditText) findViewById(R.id.password);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ob = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ob);
+            }
+        });
+        uname = et1.getText().toString();
+        password = et2.getText().toString();
     }
 }
